a0c5e9d21bd8aa6ff327e69df2ac14b0e0f7031e
wangdan <1253864861@qq.com>
2016-06-07 22:57:17 +0800

    test8

---
+++ src/collect_all_even.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function collect_all_even(collection) {
+        var i;
+        var arr=[];
+        for (i=0;i<collection.length;i++){
+            if (collection[i]%2==0){
+                arr.push(collection[i]);
+            }
+        }
+
+        return(arr);
+}
+
+module.exports = collect_all_even;
